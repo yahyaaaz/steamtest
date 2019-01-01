92cb44a3f155d48731f3096214c95137cddccc18
yahyaaaz <46232918+yahyaaaz@users.noreply.github.com>
2019-01-01 05:38:11 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame(`الكلام الي تبيه يطلع لك في الحاله `,"http://twitch.tv/y04zgamer")
+    client.user.setGame(`happy new year rex isthe best `,"http://twitch.tv/y04zgamer")
     client.user.setStatus("dnd")
   console.log(`Logged in as ${client.user.tag}!`);
 });
